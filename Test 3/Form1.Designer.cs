202fef73bb393787273fade9e977dcebad502938
JCannon1 <jessecannon1@hotmail.com>
2016-12-12 07:26:16 -0800

    Commit

--- Test 3/Form1.Designer.cs
+++ Test 3/Form1.Designer.cs
@@ -28,12 +28,288 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxNumber1 = new System.Windows.Forms.TextBox();
+            this.txtBoxNumber2 = new System.Windows.Forms.TextBox();
+            this.lblOutputQ2 = new System.Windows.Forms.Label();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblQuestion3 = new System.Windows.Forms.Label();
+            this.lblQuestion4 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblQuestion5 = new System.Windows.Forms.Label();
+            this.cboBoxSeasons = new System.Windows.Forms.ComboBox();
+            this.cboBoxMonths = new System.Windows.Forms.ComboBox();
+            this.lblQuestion6 = new System.Windows.Forms.Label();
+            this.lblQ7 = new System.Windows.Forms.Label();
+            this.btnQuestion7 = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lbl4Incorrect = new System.Windows.Forms.Label();
+            this.lbl4Correct = new System.Windows.Forms.Label();
+            this.btnMovies = new System.Windows.Forms.Button();
+            this.lblQ5Output = new System.Windows.Forms.Label();
+            this.btnShowMonths = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(487, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "My name is Jesse Cannon, my student number is 200348532, and I am going to do gre" +
+    "at on this exam.";
+            // 
+            // txtBoxNumber1
+            // 
+            this.txtBoxNumber1.Location = new System.Drawing.Point(25, 68);
+            this.txtBoxNumber1.Name = "txtBoxNumber1";
+            this.txtBoxNumber1.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxNumber1.TabIndex = 1;
+            // 
+            // txtBoxNumber2
+            // 
+            this.txtBoxNumber2.Location = new System.Drawing.Point(25, 103);
+            this.txtBoxNumber2.Name = "txtBoxNumber2";
+            this.txtBoxNumber2.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxNumber2.TabIndex = 2;
+            // 
+            // lblOutputQ2
+            // 
+            this.lblOutputQ2.AutoSize = true;
+            this.lblOutputQ2.ForeColor = System.Drawing.Color.Firebrick;
+            this.lblOutputQ2.Location = new System.Drawing.Point(22, 170);
+            this.lblOutputQ2.Name = "lblOutputQ2";
+            this.lblOutputQ2.Size = new System.Drawing.Size(164, 13);
+            this.lblOutputQ2.TabIndex = 3;
+            this.lblOutputQ2.Text = "Error: Both inputs are not integers";
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(25, 129);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(100, 23);
+            this.btnMultiply.TabIndex = 4;
+            this.btnMultiply.Text = "Multiply";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Question 2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(228, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Question3";
+            // 
+            // lblQuestion3
+            // 
+            this.lblQuestion3.AutoSize = true;
+            this.lblQuestion3.Location = new System.Drawing.Point(231, 83);
+            this.lblQuestion3.Name = "lblQuestion3";
+            this.lblQuestion3.Size = new System.Drawing.Size(65, 13);
+            this.lblQuestion3.TabIndex = 7;
+            this.lblQuestion3.Text = "lblQuestion3";
+            // 
+            // lblQuestion4
+            // 
+            this.lblQuestion4.AutoSize = true;
+            this.lblQuestion4.Location = new System.Drawing.Point(354, 52);
+            this.lblQuestion4.Name = "lblQuestion4";
+            this.lblQuestion4.Size = new System.Drawing.Size(65, 13);
+            this.lblQuestion4.TabIndex = 8;
+            this.lblQuestion4.Text = "lblQuestion4";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(357, 83);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 9;
+            // 
+            // lblQuestion5
+            // 
+            this.lblQuestion5.AutoSize = true;
+            this.lblQuestion5.Location = new System.Drawing.Point(517, 52);
+            this.lblQuestion5.Name = "lblQuestion5";
+            this.lblQuestion5.Size = new System.Drawing.Size(65, 13);
+            this.lblQuestion5.TabIndex = 10;
+            this.lblQuestion5.Text = "lblQuestion5";
+            // 
+            // cboBoxSeasons
+            // 
+            this.cboBoxSeasons.FormattingEnabled = true;
+            this.cboBoxSeasons.Items.AddRange(new object[] {
+            "Spring",
+            "Summer",
+            "Fall",
+            "WInter"});
+            this.cboBoxSeasons.Location = new System.Drawing.Point(654, 75);
+            this.cboBoxSeasons.Name = "cboBoxSeasons";
+            this.cboBoxSeasons.Size = new System.Drawing.Size(121, 21);
+            this.cboBoxSeasons.TabIndex = 11;
+            // 
+            // cboBoxMonths
+            // 
+            this.cboBoxMonths.FormattingEnabled = true;
+            this.cboBoxMonths.Location = new System.Drawing.Point(654, 114);
+            this.cboBoxMonths.Name = "cboBoxMonths";
+            this.cboBoxMonths.Size = new System.Drawing.Size(121, 21);
+            this.cboBoxMonths.TabIndex = 12;
+            // 
+            // lblQuestion6
+            // 
+            this.lblQuestion6.AutoSize = true;
+            this.lblQuestion6.Location = new System.Drawing.Point(651, 51);
+            this.lblQuestion6.Name = "lblQuestion6";
+            this.lblQuestion6.Size = new System.Drawing.Size(65, 13);
+            this.lblQuestion6.TabIndex = 13;
+            this.lblQuestion6.Text = "lblQuestion6";
+            // 
+            // lblQ7
+            // 
+            this.lblQ7.AutoSize = true;
+            this.lblQ7.Location = new System.Drawing.Point(25, 228);
+            this.lblQ7.Name = "lblQ7";
+            this.lblQ7.Size = new System.Drawing.Size(31, 13);
+            this.lblQ7.TabIndex = 14;
+            this.lblQ7.Text = "lblQ7";
+            // 
+            // btnQuestion7
+            // 
+            this.btnQuestion7.Location = new System.Drawing.Point(28, 264);
+            this.btnQuestion7.Name = "btnQuestion7";
+            this.btnQuestion7.Size = new System.Drawing.Size(97, 23);
+            this.btnQuestion7.TabIndex = 15;
+            this.btnQuestion7.Text = "Question 7";
+            this.btnQuestion7.UseVisualStyleBackColor = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(357, 111);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(100, 23);
+            this.btnLogin.TabIndex = 16;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // lbl4Incorrect
+            // 
+            this.lbl4Incorrect.AutoSize = true;
+            this.lbl4Incorrect.ForeColor = System.Drawing.Color.Firebrick;
+            this.lbl4Incorrect.Location = new System.Drawing.Point(357, 141);
+            this.lbl4Incorrect.Name = "lbl4Incorrect";
+            this.lbl4Incorrect.Size = new System.Drawing.Size(175, 13);
+            this.lbl4Incorrect.TabIndex = 17;
+            this.lbl4Incorrect.Text = "You entered the incorrect password";
+            // 
+            // lbl4Correct
+            // 
+            this.lbl4Correct.AutoSize = true;
+            this.lbl4Correct.ForeColor = System.Drawing.Color.LimeGreen;
+            this.lbl4Correct.Location = new System.Drawing.Point(357, 170);
+            this.lbl4Correct.Name = "lbl4Correct";
+            this.lbl4Correct.Size = new System.Drawing.Size(167, 13);
+            this.lbl4Correct.TabIndex = 18;
+            this.lbl4Correct.Text = "You entered the correct password";
+            // 
+            // btnMovies
+            // 
+            this.btnMovies.Location = new System.Drawing.Point(520, 83);
+            this.btnMovies.Name = "btnMovies";
+            this.btnMovies.Size = new System.Drawing.Size(75, 23);
+            this.btnMovies.TabIndex = 19;
+            this.btnMovies.Text = "Movies";
+            this.btnMovies.UseVisualStyleBackColor = true;
+            // 
+            // lblQ5Output
+            // 
+            this.lblQ5Output.AutoSize = true;
+            this.lblQ5Output.Location = new System.Drawing.Point(517, 122);
+            this.lblQ5Output.Name = "lblQ5Output";
+            this.lblQ5Output.Size = new System.Drawing.Size(39, 13);
+            this.lblQ5Output.TabIndex = 20;
+            this.lblQ5Output.Text = "Output";
+            // 
+            // btnShowMonths
+            // 
+            this.btnShowMonths.Location = new System.Drawing.Point(654, 152);
+            this.btnShowMonths.Name = "btnShowMonths";
+            this.btnShowMonths.Size = new System.Drawing.Size(121, 23);
+            this.btnShowMonths.TabIndex = 21;
+            this.btnShowMonths.Text = "Show Months";
+            this.btnShowMonths.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1021, 406);
+            this.Controls.Add(this.btnShowMonths);
+            this.Controls.Add(this.lblQ5Output);
+            this.Controls.Add(this.btnMovies);
+            this.Controls.Add(this.lbl4Correct);
+            this.Controls.Add(this.lbl4Incorrect);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnQuestion7);
+            this.Controls.Add(this.lblQ7);
+            this.Controls.Add(this.lblQuestion6);
+            this.Controls.Add(this.cboBoxMonths);
+            this.Controls.Add(this.cboBoxSeasons);
+            this.Controls.Add(this.lblQuestion5);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblQuestion4);
+            this.Controls.Add(this.lblQuestion3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.lblOutputQ2);
+            this.Controls.Add(this.txtBoxNumber2);
+            this.Controls.Add(this.txtBoxNumber1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxNumber1;
+        private System.Windows.Forms.TextBox txtBoxNumber2;
+        private System.Windows.Forms.Label lblOutputQ2;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblQuestion3;
+        private System.Windows.Forms.Label lblQuestion4;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblQuestion5;
+        private System.Windows.Forms.ComboBox cboBoxSeasons;
+        private System.Windows.Forms.ComboBox cboBoxMonths;
+        private System.Windows.Forms.Label lblQuestion6;
+        private System.Windows.Forms.Label lblQ7;
+        private System.Windows.Forms.Button btnQuestion7;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lbl4Incorrect;
+        private System.Windows.Forms.Label lbl4Correct;
+        private System.Windows.Forms.Button btnMovies;
+        private System.Windows.Forms.Label lblQ5Output;
+        private System.Windows.Forms.Button btnShowMonths;
     }
 }
 
--- Test 3/Form1.cs
+++ Test 3/Form1.cs
@@ -16,5 +16,156 @@ namespace Test_3
         {
             InitializeComponent();
         }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            lblOutputQ2.Visible = false;
+
+            Int32 txtBox1, txtBox2;
+
+            Boolean formOk = true;
+
+            if (Int32.TryParse(txtBoxNumber1.Text, out txtBox1) == false)
+            {
+                lblOutputQ2.Visible = true;
+                formOk = false;
+            }
+            else
+            {
+                lblOutputQ2.Text = GetNumber(txtBox1);
+            }
+
+            if (Int32.TryParse(txtBoxNumber2.Text, out txtBox2) == false)
+            {
+                lblOutputQ2.Visible = true;
+                formOk = false;
+            }
+            else
+            {
+                lblOutputQ2.Text = GetNumber(txtBox2);
+            }
+
+            if (formOk == true)
+            {
+
+            }
+
+        }
+        private void multiplyNumbers(object sender, EventArgs e)
+        {
+            Int32 number1 = Convert.ToInt32(txtBoxNumber1.Text);
+            Int32 number2 = Convert.ToInt32(txtBoxNumber2.Text);
+            Int32 total = (number1 * number2);
+            lblOutputQ2 = total.ToString();
+
+
+            String[] group = new String[3];
+
+            group[0] = "Merry";
+            group[1] = "Christmas";
+            group[2] = "C# is great";
+
+
+            for (var i = 0; i < group.Length; i++)
+            {
+                Console.WriteLine(group[i]);
+            }
+        }
+
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+        }
+        private String getPassword(String months)
+        {
+
+            lbl4Incorrect.Visible = false;
+
+            Int32 txtPassword;
+
+            String password = "frosty";
+
+            Boolean formOk = true;
+
+            if (Int32.TryParse(txtPassword.Text, out password) == false)
+            {
+                lbl4Incorrect.Visible = true;
+                formOk = false;
+            }
+            else
+            {
+                lbl4Incorrect.Text = getPassword(password);
+            }
+
+            if (Int32.TryParse(txtPassword.Text, out password) == false)
+            {
+                lbl4Correct.Visible = true;
+                formOk = false;
+            }
+            else
+            {
+                lbl4Correct.Text = getPassword(password);
+            }
+
+            if (formOk == true)
+            {
+
+            }
+        }
+
+        private void btnMovies_Click(object sender, EventArgs e)
+        {
+            String[] movies = new String[3];
+
+            movies[0] = "How the Grinch Stole Christmas / 1966";
+            movies[1] = "Frosty the Snowman / 1969";
+            movies[2] = "Arthur Christmas / 2011";
+
+            for (var i = 0; i < movies.Length; i++)
+            {
+                Console.WriteLine(movies[i]);
+            }
+        }
+
+        private void btnShowMonths_Click(object sender, EventArgs e)
+        {
+        }
+        private String GetMonths(String months)
+        {
+            String getMonths = months;
+
+            String cboBoxMonths = "";
+
+            String cboBoxSeasons = "";
+
+            if (cboBoxSeasons == "Spring")
+            {
+                cboBoxMonths = "March, April, May";
+            }
+            else if (cboBoxSeasons == "Summer")
+            {
+                cboBoxMonths = "June, July, August";
+            }
+            else if (cboBoxSeasons == "Fall")
+            {
+                cboBoxMonths = "September, October, November";
+            }
+            else if (cboBoxSeasons == "Winter")
+            {
+                cboBoxMonths = "December, January, February";
+            }
+            return cboBoxMonths;
+
+        }
+    }
+
+    private void btnQuestion7_Click(object sender, EventArgs e)
+    {
+        Double x = 4;
+        Double y = 12;
+
+        Int32 result = (4 / 12);
+
+        var lblQ7 = result;
     }
 }
